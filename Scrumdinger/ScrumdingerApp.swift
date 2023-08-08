95f4d6b14349ccff971d4e0ee793d8e494eef31f
torinunna <torinunna9588@gmail.com>
2023-08-08 21:38:01 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by YUJIN KWON on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
