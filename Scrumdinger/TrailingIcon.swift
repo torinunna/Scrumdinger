81a0aa506f6fc33e6ed52e72c16f8386a365a5a7
torinunna <torinunna9588@gmail.com>
2023-08-09 16:01:07 +0900

    Creating a card view

---
+++ Scrumdinger/TrailingIcon.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIcon.swift
+//  Scrumdinger
+//
+//  Created by YUJIN KWON on 2023/08/09.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
